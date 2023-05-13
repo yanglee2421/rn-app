e62ae30d7ebee052d9d8e6965289e590768910c1
ZhangSheng <yanglee2421@gmail.com>
2023-05-13 17:19:48 +0800

    app.config.ts

---
+++ app.config.ts
@@ -0,0 +1,31 @@
+import { ExpoConfig, ConfigContext } from "expo/config";
+
+export default function expoConfig(ctx: ConfigContext): ExpoConfig {
+  return {
+    ...ctx.config,
+    name: "rn-app",
+    slug: "rn-app",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+  };
+}
